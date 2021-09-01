f03ac4b1ee50d4c0d59e090cf7b1d3d76a14b2c3
luna Devops <carlosperaluna@gmail.com>
2021-09-01 08:39:17 -0400

    Actualizando rama

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -6,13 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String meessage= "Default message cambio 11";
-		//Assert.assertNotNull(meessage);
 	}
 
 }
